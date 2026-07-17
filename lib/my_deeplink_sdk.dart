bf3209fc02c5a58a67bf587c1d082a46c4ffcd59
Yug Gujarati <yug.gujarati@tecocraft.com>
2026-07-17 14:43:47 +0530

    Revert "merge working code on develop branch"
    This reverts commit c80a078b781d4422ad1913323e751f8104177388.

--- lib/my_deeplink_sdk.dart
+++ lib/my_deeplink_sdk.dart
@@ -6,7 +6,6 @@ import 'package:flutter/services.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'my_deeplink_sdk.dart';
 import 'my_deeplink_sdk_platform_interface.dart';
 import 'src/backend_client.dart';
 import 'src/models.dart';
